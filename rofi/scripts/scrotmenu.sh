c145bcee5fd2f6a2ae1f49a40fdd594e14fa605e
latived <vitalwarley@gmail.com>
2020-07-03 14:47:42 -0300

    rofi: adds menus for some utilities

---
+++ rofi/scripts/scrotmenu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# options to be displayed
+option0="screen"
+option1="area"
+option2="window"
+
+# options to be displyed
+options="$option0\n$option1\n$option2"
+
+selected="$(echo -e "$options" | rofi -lines 3 -dmenu -p "scrot")"
+case $selected in
+    $option0)
+        cd ~/Images/Screenshots/ && sleep 1 && scrot;;
+    $option1)
+        cd ~/Images/Screenshots/ && scrot -s;;
+    $option2)
+        cd ~/Images/Screenshots/ && sleep 1 && scrot -u;;
+esac
