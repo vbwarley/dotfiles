e9be1003a989d3affe1bea3b1f0995abe386a442
Warley Vital <vitalwarley@gmail.com>
2020-12-28 07:28:56 -0300

    i3: adds i3-window-cycle
    - other minor adjustments.

--- i3/toggleX.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/zsh
-mon0="eDP-1-1"
-mon1="HDMI-1-1"
-mode="1360x768"
-img="~/Images/Pessoas/RAW/1.jpg"
-xrandr --output $mon0 --auto --output $mon1 --mode $mode --left-of $mon0
-feh --bg-center $img
-i3-msg restart
