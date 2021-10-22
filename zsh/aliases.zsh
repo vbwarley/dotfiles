7c8a09c79181f58d7ee45bd217c8359d2dee1379
Warley Vital <vitalwarley@gmail.com>
2021-10-22 13:27:33 -0300

    zsh: update aliases and zshrc

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,42 +8,35 @@
 # brainstormr=~/Projects/development/planetargon/brainstormr
 # cd $brainstormr
 #
-
-alias steam="flatpak run com.valvesoftware.Steam"
-alias sps="sudo pacman -S"
+#
 
 # configs
 alias nrc="nvim /home/lativ/.config/nvim/init.vim"
-alias vrc="nvim ~/.vimrc"
+alias vrc="nvim ~/.config/nvim/init.vim"
 alias zrc="nvim ~/.zshrc"
 alias i3cfg="nvim ~/.config/i3/config"
 alias vxres="nvim ~/.Xresources"
 alias lxr="xrdb -load ~/.Xresources"
 alias xra="xrandr --auto"
 alias xrm="xrandr --output LVDS1 --auto --output VGA1 --auto --left-of LVDS1"
-# xbacklight
-alias lup="xbacklight -inc 10"
-alias ldown="xbacklight -dec 10"
-
-# folders
-alias 'pyt=cd ~/Documents/UFAL/GioconDa/Scripts/PyThings'
-alias 'rgd=cd ~/Documents/GDrive/UFAL/GioconDa/\#2\ Ciclo:\ 17-18'
-alias 'courses=cd ~/Dados/Estudos/courses'
-alias 'things=cd ~/Dados/Estudos/things'
-alias 'estudos=cd ~/Dados/Estudos'
-alias 'estudar=cd ~/Dados/Estudos && cd'
-alias 'ml=cd Study/ML; workon dl; jupyter lab'
 
 
 # commands
-alias cp="acp -g" 
-alias mv="amv -g"
+alias sps="sudo pacman -S"
+# alias cp="acp -g" 
+# alias mv="amv -g"
 alias :q="exit"
 alias ls='ls --color=tty -B'
 alias k9='kill -9'
 alias 'vi=nvim'
 alias 'vim=nvim'
-alias 'fb=rg --files ~/Books/ | grep'
+# alias 'fb=rg --files ~/Books/ | grep'
 alias checkcuda=/opt/cuda/samples/bin/x86_64/linux/release/deviceQuery
 alias momics='cd ~/dev/metabolomics/py/scripts'
 alias toledo='cd ~/dev/edge/toledo-truck-classification'
+alias dev='cd ~/dev/'
+# https://www.reddit.com/r/commandline/comments/hp1ys0/fzf_to_show_only_directories_and_cd_when_i_choose/fxllp7z/?utm_source=reddit&utm_medium=web2x&context=3
+alias f='cd $(fd --exclude data --type directory | fzf)'
+
+# sysops
+alias ssh='kitty +kitten ssh'
