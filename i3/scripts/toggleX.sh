379285a827848f9e92ab0aa5c81994c3de7a3b3d
Warley Vital <vitalwarley@gmail.com>
2021-10-28 14:32:51 -0300

    i3: update

--- i3/scripts/toggleX.sh
+++ i3/scripts/toggleX.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/zsh
-mon0="eDP-1-1"
-mon1="HDMI-1-1"
-mode="1360x768"
+mon0="eDP-1"
+mon1="HDMI-1"
+mode="1920x1080"
 img="~/Images/Pessoas/RAW/1.jpg"
 
 # This is a fix to the screen problem I have 
-xrandr --output $mon0 --off
+xrandr --output $mon1 --off
 xrandr --auto
 
 # This is just setting up a second monitor
-xrandr --output $mon0 --auto --output $mon1 --mode $mode --left-of $mon0
+xrandr --output $mon0 --output $mon1 --mode $mode --left-of $mon0
 i3-msg restart
 feh --bg-center $img
